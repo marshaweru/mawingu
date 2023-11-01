6292e0f32c591a30a71d99226b3e7cf499408626
weru <147871343+marshaweru@users.noreply.github.com>
2023-11-01 04:33:42 -0700

    Add city by using q =

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 //Variable with const keyword
 
 const apiKey = "a5fc5fcbec9f6f6e86d9ad33f154e227";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=Amsterdam&appid=a5fc5fcbec9f6f6e86d9ad33f154e227&units=metric";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=singapore";
+//We add city by using q = 
