894980523e2913ed80779deb801397a8b91c13eb
weru <147871343+marshaweru@users.noreply.github.com>
2023-11-02 04:36:13 -0700

    Select elements and update data from API

--- js/main.js
+++ js/main.js
@@ -2,4 +2,15 @@
 
 const apiKey = "a5fc5fcbec9f6f6e86d9ad33f154e227";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=singapore";
-//We add city by using q = 
+//We add city by using q = as above
+
+//Add async function and name the function
+async function checkWeather(){
+    const response = await fetch(apiUrl + `&appid=${apiKey}`);
+    var data = await response.json();
+    console.log(data);
+}
+
+checkWeather();
+
+//Update temp, city, humidity and wind information accoirding to data from API. So select these elements and update data
