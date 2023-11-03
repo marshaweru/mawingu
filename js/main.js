6ee209821a9c2772f63aa1a679ba862c67210047
weru <147871343+marshaweru@users.noreply.github.com>
2023-11-03 10:08:55 -0700

    Add dot in class name to update weather icon

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=
 const searchBox = document.querySelector(".search input");
 //Click on search btn to send city info in checkweather funct above
 const searchBtn = document.querySelector(".search button");
-const weatherIcon = document.querySelector("weather-icon");
+const weatherIcon = document.querySelector(".weather-icon");//Add dot in class name to update weather icon
 
 
 //Add async function and name the function
@@ -30,7 +30,36 @@ document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
 if(data.weather[0].main == "Clouds"){
     weatherIcon.src = "images\clouds.png";//Update source file
 }
-
+else if(data.weather[0].main == "Clear"){
+    weatherIcon.src = "images\clear.png"
+}
+else if(data.weather[0].main == "Mist"){
+    weatherIcon.src = "images\mist.png"
+}
+else if(data.weather[0].main == "Snow"){
+    weatherIcon.src = "images\snow.png"
+}
+else if(data.weather[0].main == "Drizzle"){
+    weatherIcon.src = "images\drizzle.png"
+}
+else if(data.weather[0].main == "Rain"){
+    weatherIcon.src = "images\rain.png"
+}
+else if(data.weather[0].main == "Wind"){
+    weatherIcon.src = "images\wind.png"
+}
+else if(data.weather[0].main == "Dropdown"){
+    weatherIcon.src = "images\dropdown.png"
+}
+else if(data.weather[0].main == "Humidity"){
+    weatherIcon.src = "images\humidity.png"
+}
+else if(data.weather[0].main == "Play"){
+    weatherIcon.src = "images\mist.png"
+}
+else if(data.weather[0].main == "Search"){
+    weatherIcon.src = "images\search.png"
+}
 }
 
 searchBtn.addEventListener("click", ()=>{
